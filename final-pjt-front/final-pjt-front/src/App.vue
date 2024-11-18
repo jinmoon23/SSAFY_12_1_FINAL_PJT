2e81e564aec20ad6136f77aad6b7698b68f6cd63
hyerongii <heyrong22@gmail.com>
2024-11-18 17:11:07 +0900

    update : 로그인후 메인페이지로 이동

--- final-pjt-front/final-pjt-front/src/App.vue
+++ final-pjt-front/final-pjt-front/src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    <HomeView/>
+    <nav>
+      <RouterLink :to="{name: 'HomeView'}">Home</RouterLink>
+      |
+      <RouterLink :to="{name: 'LogInView'}">로그인</RouterLink>
+      |
+      <RouterLink :to="{name: 'SignUpView'}">회원가입</RouterLink>
+    </nav>
+    <RouterView/>
   </div>
 </template>
 
 <script setup>
-import HomeView from '@/views/HomeView.vue'
+import { RouterLink, RouterView } from 'vue-router'
 
 </script>
 
