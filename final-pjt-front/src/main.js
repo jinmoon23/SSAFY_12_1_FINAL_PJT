ed4b4c6e80a053d700c5c5abf588fa009f7d0b2e
hyerongii <heyrong22@gmail.com>
2024-11-24 00:02:29 +0900

    Update : 게시글, 댓글 crud 구현 완

--- final-pjt-front/src/main.js
+++ final-pjt-front/src/main.js
@@ -6,6 +6,7 @@ import App from './App.vue'
 import router from './router'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import VueApexCharts from 'vue3-apexcharts'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const app = createApp(App)
 const pinia = createPinia()
